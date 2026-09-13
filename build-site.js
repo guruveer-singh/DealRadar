267e07ccc8c853c682861ea78329cbe081f67d18
Guruveer Singh <sguruveer12@gmail.com>
2026-09-13 12:01:02 +0530

    Add About page and update navigation

--- build-site.js
+++ build-site.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 fs.mkdirSync("dist", { recursive: true });
-for (const f of ["index.html", "app.js", "style.css", "data.js"]) {
+for (const f of ["index.html", "about.html", "app.js", "style.css", "data.js"]) {
   fs.copyFileSync(f, `dist/${f}`);
 }
